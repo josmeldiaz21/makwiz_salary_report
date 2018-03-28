219a073e6f972fffba942215d78e3f87d9bc388f
josmeldiaz21 <josmeldiaz21@hotmail.com>
2018-03-28 15:35:58 -0400

    pruebas

--- makwiz_salary_report/salary_report/report/nomina_francisco_nexia_empleador/nomina_francisco_nexia_empleador.js
+++ makwiz_salary_report/salary_report/report/nomina_francisco_nexia_empleador/nomina_francisco_nexia_empleador.js
@@ -1,18 +1,18 @@
 frappe.query_reports["Nomina Francisco-Nexia - EMPLEADOR"] = {
     "filters": [
-        {
-            "fieldname":"from_date",
-            "label": __("From Date"),
-            "fieldtype": "Date",
-            "default": frappe.datetime.month_start(),
-            "reqd": 1
-      },
-      {
-          "fieldname":"to_date",
-          "label": __("To Date"),
-          "fieldtype": "Date",
-          "default": frappe.datetime.month_end(),
-          "reqd": 1
+    {
+      "fieldname":"from_date",
+      "label": __("From Date"),
+      "fieldtype": "Date",
+      "default": frappe.datetime.month_start(),
+      "reqd": 1
+    },
+    {
+      "fieldname":"to_date",
+      "label": __("To Date"),
+      "fieldtype": "Date",
+      "default": frappe.datetime.month_end(),
+      "reqd": 1
     },
     {
       "fieldname":"company",
